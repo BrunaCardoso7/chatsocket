7fa014f98dc057ffb5e9da70336af96f78ea673b
BrunaCardoso7 <obitadrawing@gmail.com>
2024-02-03 22:13:52 -0300

    feat: initalize server code

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { createServer } from 'http';
+import dotenv from 'dotenv' ;
+import cors from 'cors'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+
+const httpServer = createServer(app);
+
+const PORT = process.env.SERV_PORT
+
+httpServer.listen( PORT, () => {
+    console.log(`
+        Servidor rodando na porta do caralho: ${PORT}
+    `)
+})
