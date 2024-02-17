9974223417e36cc69f13a0681f2a18a9fde3f51f
BrunaCardoso7 <obitadrawing@gmail.com>
2024-02-16 23:20:30 -0300

    fix: trying get userdata

--- src/controller/websocket/chat.socket.controler.ts
+++ src/controller/websocket/chat.socket.controler.ts
@@ -2,7 +2,7 @@ import { Socket } from "socket.io";
 
 export const chatPrivateControle = (socket: Socket) => {
     console.log('user conneted', socket)
-    socket.on('chatprivate', (data)=>{
-        console.log(data)
+    socket.on('chatprivate', ({name, message}) => {
+        console.log('name: '+name, 'message: '+ message)
     })
 }
