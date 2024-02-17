a595e99f6ee6b51dfcd48fc8b0bed30659939c53
BrunaCardoso7 <obitadrawing@gmail.com>
2024-02-16 23:15:46 -0300

    pass

--- src/db/conn.db.ts
+++ src/db/conn.db.ts
@@ -7,7 +7,7 @@ const secret = process.env.PASSWORD
 
 const db = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://obitadrawing:${secret}@cluster0.tknmry0.mongodb.net/?retryWrites=true&w=majority`);
+        await mongoose.connect(`mongodb+srv://obitadrawing:MkG6hcFr69u3pkRs@cluster0.tknmry0.mongodb.net/?retryWrites=true&w=majority`);
         console.log(`Conexão bem-sucedida ao banco de dados`);
     } catch (error) {
         console.error('Erro na conexão com o banco de dados:', error);
