332da5cf6cdf2320046db8233f9381a483008a66
BrunaCardoso7 <obitadrawing@gmail.com>
2024-02-16 23:20:52 -0300

    fix: conn

--- src/db/conn.db.ts
+++ src/db/conn.db.ts
@@ -8,7 +8,7 @@ const secret = process.env.PASSWORD
 const db = async () => {
     try {
         await mongoose.connect(`mongodb+srv://obitadrawing:MkG6hcFr69u3pkRs@cluster0.tknmry0.mongodb.net/?retryWrites=true&w=majority`);
-        console.log(`Conexão bem-sucedida ao banco de dados`);
+        console.log(`Conexão bem-sucedida ao banco de dados`);  
     } catch (error) {
         console.error('Erro na conexão com o banco de dados:', error);
     }
